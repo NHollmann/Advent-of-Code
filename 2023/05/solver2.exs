dfcae6ea9dc9bb8beaa0af3fa7f7e3a7d4dae0ac
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2023-12-08 19:20:05 +0100

    Part 2 is still waay to slow and memory inefficent

--- 2023/05/solver2.exs
+++ 2023/05/solver2.exs
@@ -7,17 +7,17 @@ lines = String.split(contents, "\n", trim: true)
 [seedStr | lines] = lines
 [_ | lines] = lines
 
+mc = MappingChain.parse(lines)
+
 [_, seedStr] = Regex.run(~r/^seeds: ([0-9 ]+)$/, seedStr)
 seeds = String.split(seedStr, " ", trim: true) |> Enum.map(&String.to_integer/1)
 seedRangeCount = div(length(seeds), 2) - 1
-seeds = Enum.map(0..seedRangeCount, fn idx ->
+
+Enum.map(0..seedRangeCount, fn idx ->
   start = Enum.at(seeds, idx * 2)
   len = Enum.at(seeds, idx * 2 + 1)
   start..(start + len - 1)
 end)
-|> Enum.flat_map(&Enum.to_list/1)
-
-lines
-|> MappingChain.parse
-|> SeedLocator.findNearest(seeds)
+|> Enum.map(fn r -> SeedLocator.findNearest(mc, r) end)
+|> Enum.min
 |> IO.puts
