6ebc732087dbbb6c09d593dc565f901fd7d2c423
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2023-12-26 16:39:38 +0100

    Day 8 part 2 LCM

--- 2023/08/solver2.exs
+++ 2023/08/solver2.exs
@@ -1,4 +1,22 @@
 
+defmodule Util do
+  def gcd(a, b) when a < b do
+    gcd(b, a)
+  end
+
+  def gcd(a, b) when rem(a, b) == 0  do
+    b
+  end
+
+  def gcd(a, b) do
+    gcd(b, rem(a, b))
+  end
+
+  def lcm(a, b) do
+    div(a * b, gcd(a, b))
+  end
+end
+
 {:ok, contents} = File.read("input.txt")
 lines = String.split(contents, "\n", trim: true)
 
@@ -15,28 +33,23 @@ nodes = nodeStrings
 end)
 |> Map.new()
 
-startingNodes = nodes
+nodes
 |> Map.keys()
 |> Enum.filter(&(Regex.match?(~r/^\w\wA$/, &1)))
-
-instructionStream
-|> Enum.reduce_while({startingNodes, 0}, fn op, {cur, cnt} ->
-  done = cur
-  |> Enum.map(&(Regex.match?(~r/^\w\wZ$/, &1)))
-  |> Enum.all?()
-
-  if done do
-    {:halt, cnt}
-  else
-    {l, r} = cur
-    |> Enum.map(&(Map.get(nodes, &1)))
-    |> Enum.unzip()
-
-    if op == "L" do
-      {:cont, {l, cnt + 1}}
+|> Enum.map(fn startingNode ->
+  instructionStream
+  |> Enum.reduce_while({startingNode, 0}, fn op, {cur, cnt} ->
+    if Regex.match?(~r/^\w\wZ$/, cur) do
+      {:halt, cnt}
     else
-      {:cont, {r, cnt + 1}}
+      {l, r} = Map.get(nodes, cur)
+      if op == "L" do
+        {:cont, {l, cnt + 1}}
+      else
+        {:cont, {r, cnt + 1}}
+      end
     end
-  end
+  end)
 end)
+|> Enum.reduce(1, &Util.lcm/2)
 |> IO.puts()
